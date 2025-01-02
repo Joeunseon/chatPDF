f78351578b3a791b90de54177df04c24c7a25016
eun.jo <eun.jo@mediazen.co.kr>
2025-01-02 09:33:59 +0900

    Initial project commit

---
+++ src/test/java/com/project/chat_pdf/ChatPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chat_pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
